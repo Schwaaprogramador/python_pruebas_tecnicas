6878df399b13c9dd5444bd14bd9d378c1498c53a
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-10-04 19:19:24 -0500

    Primeros archivos

---
+++ Contar_las_Palabras.py
@@ -0,0 +1,21 @@
+"""
+# PRUEBA TECNICA #
+
+En la variable 'text' dispones de una cadena de texto.
+Debes contar las palabras y devolver cuantas veces se repiten cada una.
+Ejemplo => 'nadie' aparece 2 veces
+
+"""
+
+text = "Creo que a veces es la gente de la que nadie espera nada, la que hace cosas que nadie puede imaginar."
+
+
+def to_lower_case(text):
+    lower_case = text.lower()
+    return lower_case
+    
+    
+
+lower_Case = to_lower_case(text)
+
+print(lower_Case)
---
+++ Notas_del_blog.py
@@ -0,0 +1,21 @@
+"""
+# LOS STRINGS #
+
+El backslash sirve para hacer escapes de comillas
+\n => para hacer un salgo de linea.
+\t => para hacer una TABULACION en una frase.
+
+asi como los f string, tambien esta r string. Sirve para indicar que una cadena
+es cruw, por ende no tomara los simbolos anteriores (\n . \t)
+"""
+
+print(r"C:\nombre\directorio" )
+
+"""
+Se pueden concatenar cadenas como en JS cadena1+cadena2, con el operador +
+
+
+
+"""
+
+
